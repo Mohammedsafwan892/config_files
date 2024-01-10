50d84ceab7b6fff1de2fbb33a955508109a3ba08
Mohammed Safwan <52440099+Mohammedsafwan892@users.noreply.github.com>
2024-01-10 16:22:55 +0530

    run command upload

---
+++ run_vvdn_100.sh
@@ -0,0 +1,1 @@
+sudo ./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-NR-5GC/CONF/ping_working_vvdn_3_7_12_gnb.sa.band78.fr1.273PRB.2x2.usrpn300.conf --thread-pool 12,13,14,15 --sa --reorder-thread-disable
